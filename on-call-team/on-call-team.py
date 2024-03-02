5f279467f721a85557a0979e02c99a9da1791684
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2024-03-02 00:40:48 -0600

    EVEN FASTER BUT STILL NOT WORKING AAAAA

--- on-call-team/on-call-team.py
+++ on-call-team/on-call-team.py
@@ -1,22 +1,15 @@
 import math
+import itertools
 
 def solution(arr, n, m):
-    bit_strings = []
-    broken = float("inf")
-    for i in range(int(math.pow(2, m))):
-        # Get the bit string of i
-        bit_string = ("0" * (m - len(bin(i)[2:])) + bin(i)[2:])
-        size = sum([int(x) for x in bit_string])
-        
-        # Check neighbors of subset corresponding to bit_string. 
-        # Use Hall's theorem to check for perfect matching
-        checked_elements = [i for i, x in enumerate(bit_string) if x == "1"]
-        if not valid_neighbors(arr, checked_elements, n, size):
-            broken = min(broken, size)
-    if broken == float("inf"):
-        print(m)
-    else:
-        print(broken-1)
+    for i in range(1, m+1):
+        for subset in itertools.combinations(range(m), i):
+            size = len(subset)
+            # Check neighbors of subset corresponding to bit_string. 
+            # Use Hall's theorem to check for perfect matching
+            if not valid_neighbors(arr, subset, n, size):
+                return print(size-1)
+    print(m)
 def valid_neighbors(arr, checked_elements, n, size):
     # Check if the checked elements have at least size neighbors
     workers = [False]*n
