362c0a1daf98fc190c08b5fab4ea5c165d9ae456
Ethan Beaird <98125550+spectraldecomp@users.noreply.github.com>
2024-03-02 00:34:38 -0600

    This is better but is still too slow

--- on-call-team/on-call-team.py
+++ on-call-team/on-call-team.py
@@ -2,21 +2,21 @@ import math
 
 def solution(arr, n, m):
     bit_strings = []
+    broken = float("inf")
     for i in range(int(math.pow(2, m))):
         # Get the bit string of i
         bit_string = ("0" * (m - len(bin(i)[2:])) + bin(i)[2:])
         size = sum([int(x) for x in bit_string])
-        bit_strings.append((bit_string, size))
         
-    bit_strings.sort(key=lambda x: x[1])
-    for bit_string, size in bit_strings:
         # Check neighbors of subset corresponding to bit_string. 
         # Use Hall's theorem to check for perfect matching
         checked_elements = [i for i, x in enumerate(bit_string) if x == "1"]
         if not valid_neighbors(arr, checked_elements, n, size):
-            print(size-1)
-            return size-1
-    print(m)
+            broken = min(broken, size)
+    if broken == float("inf"):
+        print(m)
+    else:
+        print(broken-1)
 def valid_neighbors(arr, checked_elements, n, size):
     # Check if the checked elements have at least size neighbors
     workers = [False]*n
--- streets-behind.py
+++ streets-behind.py
@@ -1,15 +1,22 @@
+# https://open.kattis.com/problems/streetsbehind
 import math
 
 def runs(n, k, a, b):
     ni = n
     num_runs = 0
     while ni < n + k:
-        ni = math.floor((b/a)*ni)
+        ni = min(math.floor((b/a)*ni), ni + k)
+        k_added = min(math.floor((1 - a/b)*ni), k)
         num_runs += 1
+        
     print(num_runs)
     
 t = int(input())
 
+tests = []
 for _ in range(t):
     n, k, a, b = map(int, input().split())
-    runs(n, k, a, b)   
+    tests.append((n, k, a, b))
+
+for test in tests:
+    runs(test[0], test[1], test[2], test[3])
